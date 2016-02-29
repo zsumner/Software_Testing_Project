4a992a14c6b2567ee40c32102f1ad6500803867d
Zachary Sumner <zmsumner9811@eagle.fgcu.edu>
2016-02-29 18:05:44 -0500

    Add Person / PersonTest classes

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+/**
+ * Created by John Michael on 2/8/2016.
+ */
+public class Person {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+
+    public Person(String firstName, String lastName, String address, String city, String state, String zip, String phone) {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    String phone;
+}
---
+++ src/PersonTest.java
@@ -0,0 +1,72 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by John Michael on 2/8/2016.
+ */
+public class PersonTest
+{
+    private Person person = new Person("Johnny", "Boy", "123 Sesamea Street", "Fort Myers", "FL", "33900", "2395555555");
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void testGetFirstName() throws Exception
+    {
+        person.getFirstName();
+        assertEquals("Johnny", "Johnny");
+    }
+
+    @Test
+    public void testGetLastName() throws Exception
+    {
+        person.getLastName();
+        assertEquals("Boy", "Boy");
+    }
+
+    @Test
+    public void testGetAddress() throws Exception
+    {
+        person.getAddress();
+        assertEquals("123 Sesamea Street", "123 Sesamea Street");
+    }
+
+    @Test
+    public void testGetCity() throws Exception
+    {
+        person.getCity();
+        assertEquals("Fort Myers", "Fort Myers");
+    }
+
+    @Test
+    public void testGetState() throws Exception
+    {
+        person.getState();
+        assertEquals("FL", "FL");
+    }
+
+    @Test
+    public void testGetZip() throws Exception
+    {
+        person.getZip();
+        assertEquals("33900", "33900");
+    }
+
+    @Test
+    public void testGetPhone() throws Exception
+    {
+        person.getPhone();
+        assertEquals("2395555555", "2395555555");
+    }
+}
